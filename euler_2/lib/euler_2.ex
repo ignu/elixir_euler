83f53cda0679526301c30a9175bcd7c6b32f5f3d
Len Smith <len@barrison.com>
2017-08-04 13:21:30 -0700

    Define problem 2

---
+++ euler_2/lib/euler_2.ex
@@ -0,0 +1,18 @@
+defmodule Euler2 do
+  @moduledoc """
+  Documentation for Euler2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Euler2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ euler_2/test/euler_2_test.exs
@@ -0,0 +1,8 @@
+defmodule Euler2Test do
+  use ExUnit.Case
+  doctest Euler2
+
+  test "greets the world" do
+    assert Euler2.hello() == :world
+  end
+end
